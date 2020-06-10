e365468e753d028c5fe2a40ec80f84e8d157748e
roxfordlc <roxfordlc@gmail.com>
2020-06-10 11:37:02 -0500

    Changes to the booking.component.ts

--- angular/src/app/modules/booking/booking/booking.component.ts
+++ angular/src/app/modules/booking/booking/booking.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {Booking} from '../../../data/booking.model';
+import {BookingService} from '../../../services/booking/booking.service';
+
+import {Lodging} from '../../../data/lodging.model';
+import {LodgingService} from '../../../services/lodging/lodging.service';
 
 @Component({
   selector: 'uic-booking',
   templateUrl: './booking.component.html'
 })
 export class BookingComponent implements OnInit {
-  constructor() {}
+  //private bookings: Booking[];
+  private lodgings: Lodging[];
+  constructor(private lodgingService: LodgingService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(){
+    this.getLodgings();
+  }
+  getLodgings(): void{
+    this.lodgingService.get()
+      .subscribe(lodgings => this.lodgings = lodgings )
+  }
+  
+
 }
