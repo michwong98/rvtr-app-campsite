4dd7162f974746dd0a245997025200c54c388378
agony84 <jjk198478@gmail.com>
2020-06-10 14:26:19 -0600

    adds placeholder booking e2e tests

--- angular/e2e/src/app.po.ts
+++ angular/e2e/src/app.po.ts
@@ -12,4 +12,8 @@ export class AppPage {
   getPageHeader(): Promise<string> {
     return element(by.css('uic-header')).getText() as Promise<string>;
   }
+
+  navigateToBooking(): Promise<unknown> {
+    return browser.get('/booking') as Promise<unknown>;
+  }
 }
