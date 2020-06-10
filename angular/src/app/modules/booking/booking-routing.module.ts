b89134edf77b2896e14511d731b14c56502c296b
michwong98 <michwong98@gmail.com>
2020-06-10 12:09:25 -0400

    Obersable Binding for Lodgings

--- angular/src/app/modules/booking/booking-routing.module.ts
+++ angular/src/app/modules/booking/booking-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookingComponent } from './booking/booking.component';
+import { CommonModule } from '@angular/common';
 
 const routes: Routes = [{ component: BookingComponent, path: '' }];
 
 @NgModule({
   declarations: [BookingComponent],
   exports: [RouterModule],
-  imports: [RouterModule.forChild(routes)],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule
+  ],
 })
-export class BookingRoutingModule {}
+export class BookingRoutingModule { }
--- angular/src/app/modules/booking/booking/booking.component.ts
+++ angular/src/app/modules/booking/booking/booking.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+
+import { LodgingService } from '../../../services/lodging/lodging.service';
+import { Lodging } from '../../../data/lodging.model';
+
+import { BookingService } from '../../../services/booking/booking.service';
+import { Booking } from '../../../data/booking.model';
 
 @Component({
   selector: 'uic-booking',
   templateUrl: './booking.component.html'
 })
 export class BookingComponent implements OnInit {
-  constructor() {}
+  lodgings$: Observable<Lodging[]>;
 
-  ngOnInit(): void {}
+  constructor(private lodgingService: LodgingService, private bookingService: BookingService) {
+  }
+
+  ngOnInit(): void { }
 }
