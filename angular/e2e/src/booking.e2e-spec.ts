155af8decb0082b1e948d2fa7ec8d792db6a245d
agony84 <jjk198478@gmail.com>
2020-06-11 11:01:25 -0600

    adds search functionality test

--- angular/e2e/src/booking.e2e-spec.ts
+++ angular/e2e/src/booking.e2e-spec.ts
@@ -1,25 +1,28 @@
 import { AppPage } from './app.po';
-import { browser, logging, element, by } from 'protractor';
+import { BookingPage } from './booking.po';
+import { browser, logging, element, by, Ptor, protractor } from 'protractor';
 
 describe('project rvtr-app-campsite', () => {
   let page: AppPage;
+  let booking: BookingPage;
 
   beforeEach(() => {
     page = new AppPage();
+    booking = new BookingPage();
   });
 
   /**
    * Booking e2e
    */
   it('Booking page should have header Booking',()=>{
-    page.navigateToBooking();
+    booking.navigateToBooking();
     const headerText = element(by.css('h1')).getText();
     expect(headerText).toBeTruthy();
     expect(headerText).toContain('Booking');
   });
 
   it('should have form with location label', ()=>{
-    page.navigateToBooking();
+    booking.navigateToBooking();
     const allLabels = element.all(by.css('label')).getText();
     expect(allLabels).toContain('Location');
     expect(allLabels).toContain('Check-In / Check-Out');
@@ -27,21 +30,21 @@ describe('project rvtr-app-campsite', () => {
   });
 
   it('should have button with name search', ()=>{
-    page.navigateToBooking();
+    booking.navigateToBooking();
     const buttonName = element(by.css('button')).getText();
     expect(buttonName).toBe('search')
   });
 
   it('should do something when search is clicked', ()=>{
     // TODO: after submit has been implemented, adjust test accordingly
-    page.navigateToBooking();
+    booking.navigateToBooking();
     const searchBtn = element(by.buttonText('search'));
     searchBtn.click();
     expect(searchBtn.getText()).toBe('search')
   });
 
   it('should have image', ()=>{
-    page.navigateToBooking();
+    booking.navigateToBooking();
     const image = element(by.css('img'));
     expect(image).toBeTruthy();
     const source = image.getAttribute('src');
@@ -49,7 +52,7 @@ describe('project rvtr-app-campsite', () => {
   });
 
   it('should have spotlight', ()=>{
-    page.navigateToBooking();
+    booking.navigateToBooking();
     const spotlight = element(by.className('title has-text-light is-4')).getText();
     expect(spotlight).toBe('Spotlight: My Lodging');
 
@@ -58,10 +61,21 @@ describe('project rvtr-app-campsite', () => {
   it('should contain spotlight text', ()=>{
     // TODO: when search has been implemented text should change based on 
     // location searched. 
-    page.navigateToBooking();
+    booking.navigateToBooking();
     const spotlightText = element(by.cssContainingText('p',' Lorem ipsum dolor,')).getText();
     expect(spotlightText).toBeTruthy();
     expect(spotlightText).toContain('p',' Lorem ipsum dolor,');
+  });
+
+  it('should display one property when Los Angeles is searched', ()=>{
+    // TODO: when bindings are complete adjust test to search for bound objects
+    const locationInput = element(by.name('location'));
+    const searchBtn = element(by.buttonText('search'));
+    locationInput.sendKeys('Los Angeles');
+    searchBtn.click();
+    const availLoc = element.all(by.className('tile is-child')).getText();
+    expect(availLoc).toMatch('5421 Whatever Dr, Los Angeles, CA');
+    expect(availLoc).not.toMatch('New York, NY');
   });
 
   afterEach(async () => {
