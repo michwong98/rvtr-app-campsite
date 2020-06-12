8a7cbb09298b7e9c8123d89e2c2b239170c5ffef
John Kear <39171177+agony84@users.noreply.github.com>
2020-06-12 13:32:01 -0600

    Booking e2 e (#28)
    * adds partial modal test
    
    * adds e2e tests

--- angular/e2e/src/booking.e2e-spec.ts
+++ angular/e2e/src/booking.e2e-spec.ts
@@ -1,6 +1,7 @@
 import { AppPage } from './app.po';
 import { BookingPage } from './booking.po';
 import { browser, logging, element, by, Ptor, protractor } from 'protractor';
+import { SSL_OP_NO_TICKET } from 'constants';
 
 describe('project rvtr-app-campsite', () => {
   let page: AppPage;
@@ -21,13 +22,13 @@ describe('project rvtr-app-campsite', () => {
     expect(headerText).toContain('Booking');
   });
 
-  it('should have form with location label', ()=>{
+  it('should have form with location label', (async()=>{
     booking.navigateToBooking();
     const allLabels = element.all(by.css('label')).getText();
     expect(allLabels).toMatch('Location');
     expect(allLabels).toMatch('Check-In / Check-Out');
     expect(allLabels).toMatch('Guests')
-  });
+  }));
 
   it('should have button with name search', ()=>{
     booking.navigateToBooking();
@@ -54,13 +55,14 @@ describe('project rvtr-app-campsite', () => {
     // TODO: when search has been implemented text should change based on 
     // location searched. 
     booking.navigateToBooking();
-    const spotlightText = element(by.cssContainingText('p',' Lorem ipsum dolor,')).getText();
+    const spotlightText = element.all(by.css('p')).getText()
     expect(spotlightText).toBeTruthy();
-    expect(spotlightText).toContain('p',' Lorem ipsum dolor,');
+    expect(spotlightText).toMatch('Lorem ipsum');
   });
 
   it('should display one lodging when RI is searched', ()=>{
     // TODO: when bindings are complete adjust test to search for bound objects
+    booking.navigateToBooking();
     const locationInput = element(by.name('location'));
     const searchBtn = element(by.buttonText('search'));
     locationInput.sendKeys('RI');
@@ -70,18 +72,70 @@ describe('project rvtr-app-campsite', () => {
     expect(availLoc).not.toMatch('New York, NY');
   });
 
-  it('should display modal when lodging is clicked',()=>{
-    const locationElement = element.all(by.cssContainingText('tile is-child','My Lodging'));
+  /**
+   * Booking-modal tests
+   */
+  it('should display modal when lodging is clicked', ()=>{
+    booking.navigateToBooking();
+    const locationElement = element(by.cssContainingText('.title','Great Lodge'));
+    expect(locationElement.getText()).toContain('Great Lodge');
     locationElement.click();
-    const modal = element(by.className('modal is-active'));
-    expect(modal).toBeTruthy();
-    // locationElement.click();
-    // //const modal = element(by.className('modal is-active'));
-    // expect(element(by.className('modal is-active'))).toBeTruthy();
-    // // const closeBtn = modal.element(by.className('delete'));
-    // // closeBtn.click();
-    // // expect(element(by.className('modal'))).toBeTruthy();
+    const modal = element(by.className('modal-card-title'));
+    expect(modal.getText()).toMatch('Create Booking');
+    expect(modal.isDisplayed()).toBeTruthy();
 
+  });
+
+  it('should close the modal when "x" button or "cancel" button are clicked', ()=>{
+    booking.navigateToBooking();
+    const locationElement = element(by.cssContainingText('.title','Great Lodge'));
+    expect(locationElement.getText()).toContain('Great Lodge');
+    locationElement.click();
+    const modal = element(by.className('modal-card-title'));
+    expect(modal.isDisplayed()).toBeTrue;
+    const xBtn = element(by.className('delete'));
+    xBtn.click();
+    expect(modal.isDisplayed()).toBeFalse;
+    locationElement.click();
+    expect(modal.isDisplayed()).toBeTrue;
+    const cancelBtn = element(by.buttonText('Cancel'));
+    cancelBtn.click();
+    expect(modal.isDisplayed()).toBeFalse;
+  })
+
+  it('modal should contain one card by default', ()=>{
+    booking.navigateToBooking();
+    const locationElement = element(by.cssContainingText('.title','Great Lodge'));
+    expect(locationElement.getText()).toContain('Great Lodge');
+    locationElement.click();
+    const modalCard = element.all(by.className('card ng-untouched ng-pristine ng-invalid'));
+    expect(modalCard.count()).toBe(1);
+  });
+
+  it('modal should add one card when + button is clicked', ()=>{
+    booking.navigateToBooking();
+    const locationElement = element(by.cssContainingText('.title','Great Lodge'));
+    expect(locationElement.getText()).toContain('Great Lodge');
+    locationElement.click();
+    var modalCard = element.all(by.className('card ng-untouched ng-pristine ng-invalid'));
+    expect(modalCard.count()).toBe(1);
+    const plusBtn = element(by.className('fas fa-plus'));
+    plusBtn.click();
+    modalCard = element.all(by.className('card ng-untouched ng-pristine ng-invalid'));
+    expect(modalCard.count()).toBe(2);
+  });
+
+  it('modal should remove card when Remove button is clicked', ()=>{
+    booking.navigateToBooking();
+    const locationElement = element(by.cssContainingText('.title','Great Lodge'));
+    expect(locationElement.getText()).toContain('Great Lodge');
+    locationElement.click();
+    var modalCard = element.all(by.className('card ng-untouched ng-pristine ng-invalid'));
+    expect(modalCard.count()).toBe(1);
+    const removeBtn = element(by.buttonText('Remove'));
+    removeBtn.click();
+    modalCard = element.all(by.className('card ng-untouched ng-pristine ng-invalid'));
+    expect(modalCard.count()).toBe(0);
   });
 
   afterEach(async () => {
