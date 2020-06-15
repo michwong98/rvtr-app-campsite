b39365bc92220eaf0c4b3fbfbf7e1cb5290cfe77
agony84 <jjk198478@gmail.com>
2020-06-15 08:23:02 -0600

    validation service test

--- angular/src/app/services/validation/validation.service.spec.ts
+++ angular/src/app/services/validation/validation.service.spec.ts
@@ -1,16 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ValidationService } from './validation.service';
+import { AbstractControl } from '@angular/forms';
 
 describe('ValidationService', () => {
   let service: ValidationService;
-
+  let controlFake: Partial<AbstractControl>;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    controlFake = {value: 'value'};
+    TestBed.configureTestingModule({
+      providers: [ValidationService,
+        {provide: AbstractControl, useValue: <AbstractControl>controlFake}],
+    });
     service = TestBed.inject(ValidationService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
 });
