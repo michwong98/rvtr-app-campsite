58e268ef93145e00658a57e8c72a1170139ae8c6
michwong98 <47164640+michwong98@users.noreply.github.com>
2020-06-16 17:33:32 -0400

    Lint issues. (#35)
    * More linting fixes.

--- angular/e2e/src/booking.po.ts
+++ angular/e2e/src/booking.po.ts
@@ -1,7 +1,6 @@
 import { browser, by, element } from 'protractor';
 
 export class BookingPage {
-  
   navigateToBooking(): Promise<unknown> {
     return browser.get('/booking') as Promise<unknown>;
   }
