d700ec28a6c654137ae4c2b8acbbc62a3bae5b57
michwong98 <michwong98@gmail.com>
2020-06-16 16:58:31 -0400

    Revert "account binding and services"
    This reverts commit ba5ac97504ade5680d3b232022536c964492f98e.

--- angular/src/app/modules/account/account-routing.module.ts
+++ angular/src/app/modules/account/account-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountComponent } from './account/account.component';
-import { CommonModule } from '@Angular/common';
 
 const routes: Routes = [{ component: AccountComponent, path: '' }];
 
 @NgModule({
   declarations: [AccountComponent],
   exports: [RouterModule],
-  imports: [RouterModule.forChild(routes), CommonModule],
+  imports: [RouterModule.forChild(routes)],
 })
 export class AccountRoutingModule {}
--- angular/src/app/modules/account/account/account.component.ts
+++ angular/src/app/modules/account/account/account.component.ts
@@ -1,44 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { AccountService } from '../../../services/account/account.service';
-import { Account } from '../../../data/account.model';
-import { Review } from '../../../data/review.model';
-import { Booking } from '../../../data/booking.model';
-
 
 @Component({
   selector: 'uic-account',
   templateUrl: './account.component.html',
 })
 export class AccountComponent implements OnInit {
-  
-  data: Account;
-  reviews: Review[]; 
-  bookings: Booking[];
-  constructor(private AccSer: AccountService) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.dummyGet();
-    this.dummyGetReviews();
-    this.dummyGetBookings();
-  }
-
-  dummyGetBookings(){
-    this.AccSer.dummyGetBookings("yo").subscribe(books => this.bookings = books);
-    console.log(this.bookings);
-  }
-
-  dummyGet(){
-    this.AccSer.get("hey dummy").subscribe(data => this.data = data);
-    this.obscure();
-  }
-
-  obscure(){
-    for(let i = 0; i < this.data.payments.length; i++){
-      this.data.payments[i].cardNumber = "***********"+ this.data.payments[i].cardNumber.substring(11,16);
-    }
-
-  }
-  dummyGetReviews(){
-    this.AccSer.dummyGetReveiws("hi").subscribe( val => this.reviews = val);
-  }
+  ngOnInit(): void {}
 }
