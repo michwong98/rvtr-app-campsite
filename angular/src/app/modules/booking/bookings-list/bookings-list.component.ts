2800b4b800def72a3d8b3d39a3352880c90bf026
Mohcka <mikeduels@yahoo.com>
2020-06-19 09:09:24 -0700

    Subscribes to service 📰

--- angular/src/app/modules/booking/bookings-list/bookings-list.component.ts
+++ angular/src/app/modules/booking/bookings-list/bookings-list.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bookings-list.component.scss']
 })
 export class BookingsListComponent implements OnInit {
+  /**
+   * Observable that retreives a list of bookings
+   * from the `BookingService`
+   */
+  bookings$: Observable<Booking[]>;
+  /**
+   * Limits the amount of records to be returned
+   */
+  limit = 5;
+  /**
+   * The amount of records that should be skipped from the api server
+   */
+  offset = 5;
 
   constructor(private bookingsService: BookingService) { }
 
   ngOnInit(): void {
+    this.getBookings();
+  }
+
+  /**
+   * Subscribes to the `BookingService.getPartial` observable,
+   * fetching a list of `Booking`s from teh `BookingApi`.
+   */
+  getBookings(): void {
+    this.bookings$ = this.bookingsService.getPartial(`${this.limit}`, `${this.offset}`);
   }
 
 }
