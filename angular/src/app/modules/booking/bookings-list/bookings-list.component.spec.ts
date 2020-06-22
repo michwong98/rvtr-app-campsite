6dd0ac73f0bf2ac99bad979d49be078dfda42735
agony84 <jjk198478@gmail.com>
2020-06-22 16:20:49 -0600

    adds testing to bookings list component

--- angular/src/app/modules/booking/bookings-list/bookings-list.component.spec.ts
+++ angular/src/app/modules/booking/bookings-list/bookings-list.component.spec.ts
@@ -2,14 +2,33 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { BookingsListComponent } from './bookings-list.component';
+import { Booking } from 'src/app/data/booking.model';
+import { subscribeOn } from 'rxjs/operators';
+import { BookingService } from 'src/app/services/booking/booking.service';
+import { Observable, of } from 'rxjs';
+import { mockBookings } from '../mock-booking-data';
+import { BookingComponent } from '../booking/booking.component';
+import { Data } from '@angular/router';
 
 describe('BookingsListComponent', () => {
   let component: BookingsListComponent;
   let fixture: ComponentFixture<BookingsListComponent>;
+  const bookingMock: Partial<Booking> = {
+    id: '0',
+  };
+
+  const bookingServiceMock = {
+    get(id?: string) {
+      var bookings: Observable<Booking[]>;
+      return bookings;
+    },
+    delete:  ()  => { return {subscribe: ()=> {} } },
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [BookingsListComponent],
+      providers: [{provide: BookingService, useValue: bookingServiceMock}],
       imports: [HttpClientTestingModule]
     })
       .compileComponents();
@@ -23,5 +42,26 @@ describe('BookingsListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should delete booking', async (() => {
+    const bookingSpy = spyOn(bookingServiceMock, 'delete').and.returnValue({ subscribe: () => {
+      const getBookingSpy = spyOn(component, 'getBookings');
+      component.deleteBooking(bookingMock as Booking);
+      fixture.detectChanges();
+      expect(bookingSpy).toHaveBeenCalled();
+    } });
+  }));
+
+  it('should get bookings', () => {
+    const bookingSpy = spyOn(component, 'getBookings');
+    component.getBookings();
+    expect(bookingSpy).toHaveBeenCalled();
+  });
+
+  it('should edit bookings', () => {
+    const bookingSpy = spyOn(component, 'editBooking');
+    const result = component.editBooking(bookingMock as Booking);
+    expect(bookingSpy).toHaveBeenCalled();
   });
 });
